318290ff26bd32c6a2277a34b1d5e3d6e64da628
oscarAlvarezR <56967950+oscarAlvarezR@users.noreply.github.com>
2020-03-22 18:17:07 +0100

    final

---
+++ M6UF2ACT8/src/m6uf2act8/M6UF2ACT8.java
@@ -0,0 +1,10 @@
+package m6uf2act8;
+
+public class M6UF2ACT8 {
+
+    public static void main(String[] args) {
+        
+        Interfaz interfaz = new Interfaz();
+        interfaz.setVisible(true);
+    }
+}
---
+++ M6UF2ACT8/src/m6uf2act8/vehicle.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m6uf2act8;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.*;
+/**
+ *
+ * @author jaade
+ */
+@Entity
+public class vehicle implements Serializable {
+    
+    @Id @GeneratedValue
+    private long id;
+    
+    
+    private String model;
+    private propietari propietari;
+    private Date ultimaRevisio;
+    private boolean electric;
+    
+    public vehicle () {
+    }
+    
+    public vehicle(String model, propietari propietari, Date ultimaRevisio, boolean electric) {
+        
+        this.model = model;
+        this.propietari = propietari;
+        this.ultimaRevisio = ultimaRevisio;
+        this.electric = electric;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public String getModel() {
+        return model;
+    }
+    
+    public propietari getPropietari() {
+        return propietari;
+    }
+    
+    public Date getUltimaRevisio() {
+        return ultimaRevisio;
+    }
+    
+    public boolean getElectric() {
+        return electric;
+    }
+    
+    @Override
+    public String toString() {
+        
+        if (electric) {
+            
+            return "Id: " + id + ", Model: " + model + ", Propietari: " + propietari.getNom() + ", Ultima revisio: " + ultimaRevisio + ", Electric: SI.";
+            
+        }
+        
+        return "Id: " + id + ", Model: " + model + ", Propietari: " + propietari.getNom() + ", Ultima revisio: " + ultimaRevisio + ", Electric: NO.";
+    }
+}
